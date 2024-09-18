3a5d85fc5a45f75ca39c0b4aff75633c3a121b63
Serhii Ivanenko <serhii.ivanenko@tecnico.ulisboa.pt>
2024-09-18 14:10:36 +0100

    Fixing links.

--- fake-worker/deploy-swarm.sh
+++ fake-worker/deploy-swarm.sh
@@ -4,17 +4,12 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-if [[ -z "${ARGO_HOME}" ]]; then
-    echo "ARGO_HOME is not defined. Exiting..."
-    exit 1
-fi
-
 if [[ -z "${JAVA_HOME}" ]]; then
     echo "JAVA_HOME is not defined. Exiting..."
     exit 1
 fi
 
-cd "$ARGO_HOME/../benchmarks/fake-worker" || {
+cd "$DIR" || {
     echo "Redirection fails!"
     exit 1
 }
