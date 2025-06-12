e9a86e56a7ab4c9f2297746ec4a87a8fdc30ef97
Serhii Ivanenko <serhii.ivanenko@tecnico.ulisboa.pt>
2025-06-12 12:50:32 +0100

    Considering actual functon size in memory for single invocation memory manager.

--- azure-dataset/src/main/java/org/graalvm/argo/dataset/execution/mw/memory/SingleInvocationMemoryManager.java
+++ azure-dataset/src/main/java/org/graalvm/argo/dataset/execution/mw/memory/SingleInvocationMemoryManager.java
@@ -2,33 +2,38 @@ package org.graalvm.argo.dataset.execution.mw.memory;
 
 import org.graalvm.argo.dataset.execution.Environment;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SingleInvocationMemoryManager implements AbstractMemoryManager {
 
-    private final AtomicInteger concurrentInvocations;
+    private final AtomicInteger memoryConsumption;
+    private final Map<String, Integer> functionMemory;
 
     public SingleInvocationMemoryManager() {
-        this.concurrentInvocations = new AtomicInteger(0);
+        this.memoryConsumption = new AtomicInteger(0);
+        this.functionMemory = new HashMap<>();
     }
 
     @Override
     public void startRequest(String ownerName, String functionName, int memory) {
-        concurrentInvocations.incrementAndGet();
+        functionMemory.putIfAbsent(ownerName + "_" + functionName, memory);
+        memoryConsumption.set(memoryConsumption.get() + memory);
     }
 
     @Override
     public void finishRequest(String ownerName, String functionName) {
-        concurrentInvocations.decrementAndGet();
+        memoryConsumption.set(memoryConsumption.get() - functionMemory.get(ownerName + "_" + functionName));
     }
 
     @Override
     public boolean canAccommodateRequest(String ownerName, String functionName, int memory) {
-        return getCurrentMemoryConsumption() + Environment.VM_MEMORY <= Environment.MAX_MEMORY_PER_WORKER_MB;
+        return getCurrentMemoryConsumption() + memory <= Environment.MAX_MEMORY_PER_WORKER_MB;
     }
 
     @Override
     public int getCurrentMemoryConsumption() {
-        return concurrentInvocations.get() * Environment.VM_MEMORY;
+        return memoryConsumption.get();
     }
 }
