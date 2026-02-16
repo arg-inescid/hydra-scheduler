de42fba4b081dff9eb3e516c1c1323dd77da8f3d
Tiago Deane Motta e Silva <tiagodeane@tecnico.ulisboa.pt>
2026-02-16 15:40:12 +0000

    followup from previous commit

--- azure-dataset/src/main/java/org/graalvm/argo/dataset/generator/FunctionInfoStorage.java
+++ azure-dataset/src/main/java/org/graalvm/argo/dataset/generator/FunctionInfoStorage.java
@@ -13,12 +13,18 @@ public class FunctionInfoStorage {
 
     /* Key - function */
     public static final Map<String, Integer> DURATIONS = new HashMap<>();
+    /* Key - function */
+    public static final Map<String, Integer> P99_DURATIONS = new HashMap<>(); // currently unused
+    /* Key - function */
+    public static final Map<String, Integer> COMPRESSED_MAPPING = new HashMap<>();
     /* Key - app */
     public static final Map<String, Integer> MEMORIES = new HashMap<>();
 
     public static void fillFunctionData(String datasetId) {
         DURATIONS.clear();
+        P99_DURATIONS.clear();
         MEMORIES.clear();
+        COMPRESSED_MAPPING.clear();
         fillDurations(datasetId);
         fillMemories(datasetId);
     }
@@ -38,6 +44,8 @@ public class FunctionInfoStorage {
                 String function = splitRow[2];
                 int averageDuration = Integer.parseInt(splitRow[3]);
                 DURATIONS.put(function, averageDuration);
+                int p99Duration = Integer.parseInt(splitRow[12]);
+                P99_DURATIONS.put(function, p99Duration);
             }
             br.close();
         } catch (IOException ioe) {
